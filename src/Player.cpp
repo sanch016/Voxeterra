5aa88d75b63c8dd1181ee4f3e360ffd8a6794afd
sanch016 <qwqwqw168168168@gmail.com>
2026-07-14 21:58:44 +0300

    Smooth auto step-up: only at foot level when block above is air

--- src/Player.cpp
+++ src/Player.cpp
@@ -82,19 +82,8 @@ void Player::update(float deltaTime, const World& world) {
         m_velocity.y -= m_gravity * deltaTime;
 
         glm::vec3 oldPos = m_position;
-
-        // X axis with step-up
         m_position.x += m_velocity.x * deltaTime;
-        if (hasCollision(world, getAABB())) {
-            float savedY = m_position.y;
-            m_position.y += BLOCK_SIZE;
-            if (hasCollision(world, getAABB())) {
-                m_position.y = savedY;
-                resolveCollisionsAxis(world, 0);
-            } else {
-                m_velocity.y = 0.0f;
-            }
-        }
+        resolveCollisionsAxis(world, 0);
 
         m_position.y += m_velocity.y * deltaTime;
         resolveCollisionsAxis(world, 1);
@@ -102,18 +91,8 @@ void Player::update(float deltaTime, const World& world) {
             m_grounded = true;
         }
 
-        // Z axis with step-up
         m_position.z += m_velocity.z * deltaTime;
-        if (hasCollision(world, getAABB())) {
-            float savedY = m_position.y;
-            m_position.y += BLOCK_SIZE;
-            if (hasCollision(world, getAABB())) {
-                m_position.y = savedY;
-                resolveCollisionsAxis(world, 2);
-            } else {
-                m_velocity.y = 0.0f;
-            }
-        }
+        resolveCollisionsAxis(world, 2);
 
         if (m_grounded && IsKeyPressed(KEY_SPACE)) {
             m_velocity.y = m_jumpVelocity;
@@ -123,58 +102,78 @@ void Player::update(float deltaTime, const World& world) {
 }
 
 void Player::resolveCollisionsAxis(const World& world, int axis) {
-    AABB box = getAABB();
+    bool stepped;
+    do {
+        stepped = false;
+        AABB box = getAABB();
 
-    int minX = static_cast<int>(std::floor(box.min.x / BLOCK_SIZE));
-    int maxX = static_cast<int>(std::floor(box.max.x / BLOCK_SIZE));
-    int minY = static_cast<int>(std::floor(box.min.y / BLOCK_SIZE));
-    int maxY = static_cast<int>(std::floor(box.max.y / BLOCK_SIZE));
-    int minZ = static_cast<int>(std::floor(box.min.z / BLOCK_SIZE));
-    int maxZ = static_cast<int>(std::floor(box.max.z / BLOCK_SIZE));
+        int minX = static_cast<int>(std::floor(box.min.x / BLOCK_SIZE));
+        int maxX = static_cast<int>(std::floor(box.max.x / BLOCK_SIZE));
+        int minY = static_cast<int>(std::floor(box.min.y / BLOCK_SIZE));
+        int maxY = static_cast<int>(std::floor(box.max.y / BLOCK_SIZE));
+        int minZ = static_cast<int>(std::floor(box.min.z / BLOCK_SIZE));
+        int maxZ = static_cast<int>(std::floor(box.max.z / BLOCK_SIZE));
 
-    for (int x = minX; x <= maxX; ++x) {
-        for (int y = minY; y <= maxY; ++y) {
-            for (int z = minZ; z <= maxZ; ++z) {
-                BlockType block = world.getBlock(x, y, z);
-                if (!isBlockSolid(block)) continue;
+        for (int x = minX; x <= maxX && !stepped; ++x) {
+            for (int y = minY; y <= maxY && !stepped; ++y) {
+                for (int z = minZ; z <= maxZ && !stepped; ++z) {
+                    BlockType block = world.getBlock(x, y, z);
+                    if (!isBlockSolid(block)) continue;
 
-                float bx = x * BLOCK_SIZE, by = y * BLOCK_SIZE, bz = z * BLOCK_SIZE;
-                float bs = BLOCK_SIZE;
-                AABB voxel = {
-                    glm::vec3(bx, by, bz),
-                    glm::vec3(bx + bs, by + bs, bz + bs)
-                };
+                    float bx = x * BLOCK_SIZE, by = y * BLOCK_SIZE, bz = z * BLOCK_SIZE;
+                    float bs = BLOCK_SIZE;
+                    AABB voxel = {
+                        glm::vec3(bx, by, bz),
+                        glm::vec3(bx + bs, by + bs, bz + bs)
+                    };
 
-                if (box.min.x >= voxel.max.x || box.max.x <= voxel.min.x) continue;
-                if (box.min.y >= voxel.max.y || box.max.y <= voxel.min.y) continue;
-                if (box.min.z >= voxel.max.z || box.max.z <= voxel.min.z) continue;
+                    if (box.min.x >= voxel.max.x || box.max.x <= voxel.min.x) continue;
+                    if (box.min.y >= voxel.max.y || box.max.y <= voxel.min.y) continue;
+                    if (box.min.z >= voxel.max.z || box.max.z <= voxel.min.z) continue;
 
-                float halfW = m_width * 0.5f;
-
-                if (axis == 0) {
-                    if (m_velocity.x > 0.0f)
-                        m_position.x = voxel.min.x - halfW;
-                    else if (m_velocity.x < 0.0f)
-                        m_position.x = voxel.max.x + halfW;
-                    m_velocity.x = 0.0f;
-                } else if (axis == 1) {
-                    if (m_velocity.y < 0.0f) {
-                        m_position.y = voxel.max.y;
-                        m_grounded = true;
-                    } else if (m_velocity.y > 0.0f) {
-                        m_position.y = voxel.min.y - m_height;
+                    if (axis != 1) {
+                        float blockTop = by + bs;
+                        float playerFoot = m_position.y;
+                        if (blockTop > playerFoot - 0.001f &&
+                            blockTop < playerFoot + bs + 0.001f) {
+                            int aboveY = y + 1;
+                            BlockType above = world.getBlock(x, aboveY, z);
+                            if (!isBlockSolid(above)) {
+                                m_position.y = blockTop;
+                                m_velocity.y = 0.0f;
+                                stepped = true;
+                                break;
+                            }
+                        }
                     }
-                    m_velocity.y = 0.0f;
-                } else {
-                    if (m_velocity.z > 0.0f)
-                        m_position.z = voxel.min.z - halfW;
-                    else if (m_velocity.z < 0.0f)
-                        m_position.z = voxel.max.z + halfW;
-                    m_velocity.z = 0.0f;
-                }
 
-                box = getAABB();
+                    float halfW = m_width * 0.5f;
+
+                    if (axis == 0) {
+                        if (m_velocity.x > 0.0f)
+                            m_position.x = voxel.min.x - halfW;
+                        else if (m_velocity.x < 0.0f)
+                            m_position.x = voxel.max.x + halfW;
+                        m_velocity.x = 0.0f;
+                    } else if (axis == 1) {
+                        if (m_velocity.y < 0.0f) {
+                            m_position.y = voxel.max.y;
+                            m_grounded = true;
+                        } else if (m_velocity.y > 0.0f) {
+                            m_position.y = voxel.min.y - m_height;
+                        }
+                        m_velocity.y = 0.0f;
+                    } else {
+                        if (m_velocity.z > 0.0f)
+                            m_position.z = voxel.min.z - halfW;
+                        else if (m_velocity.z < 0.0f)
+                            m_position.z = voxel.max.z + halfW;
+                        m_velocity.z = 0.0f;
+                    }
+
+                    box = getAABB();
+                }
             }
         }
-    }
+    } while (stepped);
 }
